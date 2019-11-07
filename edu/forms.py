692b0b0a5a20d743e9a1aa74c3fd88833ebd632f
alrz1999 <alrz1999@gmail.com>
2019-11-07 15:26:58 +0330

    second commit

---
+++ edu/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.', label='نام')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.', label='نام خانوادگی')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.', label='ایمیل')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
---
+++ edu/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from edu.views import *
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('register',signup,name='signup')
+]
