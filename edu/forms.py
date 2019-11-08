4e3ce8bb8d703cc3d1d0f75828d5556e1d2afd21
Utop <pahang1378@gmail.com>
2019-11-08 17:06:18 +0330

    search course

--- edu/forms.py
+++ edu/forms.py
@@ -44,7 +44,7 @@ class ContactUsForm(ModelForm):
 
 class ProfileSettingForm(forms.Form):
     first_name = forms.CharField(max_length=100, required=False)
-    last_name = forms.CharField(max_length=100, required=False)
+    last_name = forms.CharField(max_length=100, required=True)
     profile_image = forms.ImageField(required=False)
 
 
